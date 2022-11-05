2a2fb1f608e75dc71f2028d5825bdea8d374d07d
arpit7786 <arpit97singh@gmail.com>
2022-11-05 23:03:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/a7minworkout/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.a7minworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val flStartButton: FrameLayout = findViewById(R.id.flButtonStart)
+        flStartButton.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity,
+                "Here we'll start the exercise",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+    }
+}
